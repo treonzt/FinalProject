5adad6d053b49be2ba6bf1ebff5be2088f8b07a8
treonzt <103697424+treonzt@users.noreply.github.com>
2022-04-28 19:13:06 -0400

    Add files via upload

---
+++ TeamInfo2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject_Group1.Models2
+{
+    public class TeamInfo2
+    {
+        public int Id { get; set; }
+        public string fullName { get; set; }
+        public string favoriteColor { get; set; }
+        public string favoriteAnimal { get; set; }
+
+        public string favoriteDrink { get; set; }
+    }
+}
---
+++ TeamInfoContext2.cs
@@ -0,0 +1,23 @@
+﻿using FinalProject_Group1.Models2;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject_Group1.Data2
+{
+    public class TeamInfoContext2 : DbContext
+    {
+        public TeamInfoContext2(DbContextOptions<TeamInfoContext2> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TeamInfo2>().HasData(
+                new TeamInfo2 { Id = 1, fullName = "Zach Treon", favoriteColor = "Green", favoriteAnimal = "Parakeet", favoriteDrink = "Root Beer" }
+                );
+        }
+
+        public DbSet<TeamInfo2> TeamInfo2 { get; set; }
+    }
+}
