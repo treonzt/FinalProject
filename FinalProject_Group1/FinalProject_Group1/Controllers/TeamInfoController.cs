dce72bf62b5b13d374851f4a6bf02b4415e2e256
saxenaii <saxenaii@mail.uc.edu>
2022-04-28 17:39:44 -0400

    Created CRUD for Team Info Table

--- FinalProject_Group1/FinalProject_Group1/Controllers/TeamInfoController.cs
+++ FinalProject_Group1/FinalProject_Group1/Controllers/TeamInfoController.cs
@@ -1,5 +1,6 @@
 ﻿using FinalProject_Group1.Data;
 using FinalProject_Group1.Interfaces;
+using FinalProject_Group1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,25 +12,26 @@ namespace FinalProject_Group1.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+
+    // @author: Ishani Saxena
     public class TeamInfoController : ControllerBase
     {
        
         private readonly ILogger<TeamInfoController> _logger;
-        //private readonly TeamInfoContext _context;
         private readonly ITeamInfoContextDAO _context;
-
+        // @author: Ishani Saxena
         public TeamInfoController(ILogger<TeamInfoController> logger, ITeamInfoContextDAO context)
         {
             _logger = logger;
             _context = context;
         }
-
+        // @author: Ishani Saxena
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(_context.GetAllTeamInfos());
         }
-
+        // @author: Ishani Saxena
         [HttpGet("id")]
         public IActionResult Get(int id)
         {
@@ -39,6 +41,56 @@ namespace FinalProject_Group1.Controllers
                 return NotFound(id);
             }
             return Ok(team);
+        }
+        // @author: Ishani Saxena
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var result = _context.RemoveTeamById(id);
+            if (result == null)
+            {
+                return NotFound(id);
+            }
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occurred.");
+            }
+
+            return Ok();
+        }
+        // @author: Ishani Saxena
+        [HttpPut]
+        public IActionResult Put(TeamInfo team)
+        {
+            var result = _context.UpdateTeam(team);
+            if (result == null)
+            {
+                return NotFound(team.Id);
+            }
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occurred.");
+            }
+
+            return Ok();
+
+        }
+        // @author: Ishani Saxena
+        [HttpPost]
+        public IActionResult Post(TeamInfo team)
+        {
+            var result = _context.Add(team);
+
+            if (result == null)
+            {
+                return StatusCode(500, "Item already exists!");
+            }
+            if(result == 0)
+            {
+                return StatusCode(500, "An error occurred.");
+            }
+
+            return Ok();
         }
     }
 }
